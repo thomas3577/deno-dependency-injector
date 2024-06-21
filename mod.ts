272fa2033b525b9a4014b70f3eadcb74f7537934
Thomas Huber <thomas3577@gmx.net>
2024-06-21 22:59:55 +0200

    fix: Added missing export

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
 export { Bootstrapped, Injectable } from './decorators.ts';
 export type { InjectionOptions } from './decorators.ts';
-export { bootstrap, Injector } from './injector.ts';
+export { bootstrap, Injector, setInjectionMetadata } from './injector.ts';
 export { Reflect } from '@dx/reflect';
